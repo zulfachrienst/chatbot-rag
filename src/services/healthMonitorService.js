53db5a7d51ba289ee6edebf4c80eb9d090ce04f7
Muhammad Zulfachrie Nasution <skynight0704@gmail.com>
2025-06-16 20:12:05 +0700

    modified:   src/services/healthMonitorService.js

--- src/services/healthMonitorService.js
+++ src/services/healthMonitorService.js
@@ -7,6 +7,7 @@ async function testHuggingFaceOnce() {
     const { HfInference } = require('@huggingface/inference');
     const hf = new HfInference(process.env.HUGGINGFACE_API_KEY);
     const result = await hf.featureExtraction({
+        provider: "hf-inference",
         model: 'sentence-transformers/all-MiniLM-L6-v2',
         inputs: 'test embedding'
     });
