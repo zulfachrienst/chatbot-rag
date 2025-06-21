dd4d6efdeae99a17f51a5f605c6598edd0d339a2
Muhammad Zulfachrie Nasution <skynight0704@gmail.com>
2025-06-21 07:13:50 +0700

    modified:   src/utils/firebaseStorage.js

--- src/utils/firebaseStorage.js
+++ src/utils/firebaseStorage.js
@@ -6,15 +6,18 @@ async function uploadImageBuffer(buffer, mimetype, productId) {
     const filename = `products/${productId}/${uuidv4()}`;
     const file = bucket.file(filename);
 
+    // Generate token
+    const token = uuidv4();
+
     await file.save(buffer, {
         metadata: {
             contentType: mimetype,
-            metadata: { firebaseStorageDownloadTokens: uuidv4() }
+            metadata: { firebaseStorageDownloadTokens: token }
         }
     });
 
-    // Generate public URL
-    const url = `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${encodeURIComponent(filename)}?alt=media`;
+    // Generate public URL with token
+    const url = `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${encodeURIComponent(filename)}?alt=media&token=${token}`;
     return url;
 }
 
